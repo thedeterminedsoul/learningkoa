15332d7ab93b36961e17872d64526daad0013cb6
Pawan Tejwani <>
2020-04-09 03:46:18 +0530

    adding middleware definition

---
+++ src/middleware/error.middleware.js
@@ -0,0 +1,14 @@
+
+// centralized error handler for application
+module.exports = async (ctx, next) => {
+    try {
+        await next();
+    } catch(err) {
+        if (err.status >= 500) console.log('Error handler:', err);
+        ctx.status = err.status || 500;
+        ctx.body = {
+            status: 'failed',
+            message: err.message || 'Internal server error',
+        };
+    }
+};
