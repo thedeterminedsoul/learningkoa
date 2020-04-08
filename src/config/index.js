2b59f2c35669a8681a00fd9cdcc096ed58906b0c
Pawan Tejwani <>
2020-04-09 04:13:18 +0530

    adding configuration support.

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,18 @@
+
+'use strict';
+
 // file for all project related configurations
-var dotenv = require('dotenv');
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
+const config = {};
+const basePath = path.join(__dirname, 'components');
+
+// require all the files from the components directory
+fs.readdirSync(basePath).forEach(file => {
+    const componentConfig = require(path.join(basePath, file));
+    Object.assign(config, componentConfig);
+});
+
+module.exports = config;
